5ca696930aac2d7268841573de66b90209b2cc41
Paldeep Singh <paldeepsingh21@gmail.com>
2025-04-02 20:23:33 +0800

    Open and close nav block

---
+++ main.js
@@ -0,0 +1,14 @@
+const navToggle = document.querySelector(
+  '[aria-controls="primary-navigation"]'
+);
+const primaryNav = document.querySelector("#primary-navigation");
+
+navToggle.addEventListener("click", () => {
+  const navOpened = navToggle.getAttribute("aria-expanded");
+
+  if (navOpened === "false") {
+    navToggle.setAttribute("aria-expanded", "true");
+  } else {
+    navToggle.setAttribute("aria-expanded", "false");
+  }
+});
